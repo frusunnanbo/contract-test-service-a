911c1220dbf6c6103640b5fd074df2860ee8b5d9
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-12-29 16:37:04 +0100

    Fetching stuff from the backend

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-app = require('express')();
+const app = require('express')();
+const { getStuff } = require('./apiClient');
 
 app.get('/', (request, response) => response.send('Service 2'));
+app.get('/stuff', (request, response) => getStuff()
+        .then((stuff) => response
+                .json(stuff))
+        .catch((err) =>
+                response.status(500)
+                .send(`failed to get stuff: ${err.stack}`)));
 
-app.listen(3002);
+app.listen(3002, console.log('Listening on 3002'));
