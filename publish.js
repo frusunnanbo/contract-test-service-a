6e1c457bb2de2d3481234459298f95d3e0323e17
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2020-01-02 20:02:27 +0100

    Adds commit hash to pact version

--- publish.js
+++ publish.js
@@ -1,11 +1,14 @@
 const pact = require('@pact-foundation/pact-node');
 const path = require('path');
+const { execSync } = require('child_process');
+
+const gitVersion = execSync('git rev-parse HEAD').toString().trim();
 
 const opts = {
     pactFilesOrDirs: [path.resolve(__dirname, './pacts/')],
     pactBroker: 'https://frusunnanbo.pact.dius.com.au/',
     pactBrokerToken: process.env.PACT_BROKER_TOKEN,
-    consumerVersion: '1.0.0'
+    consumerVersion: `1.0.0-${gitVersion}`
 };
 
 pact.publishPacts(opts)
