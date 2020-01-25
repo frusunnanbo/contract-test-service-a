56a36fc2703a6bd744217c35cdafc1b1fce7310a
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2020-01-25 09:36:59 +0100

    Better pact for filtering

--- __tests__/apiClient.pact.spec.js
+++ __tests__/apiClient.pact.spec.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const {Pact, Matchers} = require('@pact-foundation/pact');
+const {like} = Matchers;
 const {fetchAnimals} = require('../apiClient');
 
 const provider = new Pact({
@@ -18,6 +19,7 @@ describe('Contract with Service C', () => {
 
         beforeEach(() => {
             const interaction = {
+                state: 'there are 2 hedgehogs and 1 cat',
                 uponReceiving: 'a request for animals',
                 withRequest: {
                     path: '/animals',
@@ -25,21 +27,30 @@ describe('Contract with Service C', () => {
                     query: 'kind=hedgehog'
                 },
                 willRespondWith: {
-                    body: Matchers.eachLike({
-                                name: "Joy",
-                                kind: "hedgehog",
-                                image: {
-                                    path: "/path/to/image"
-                                },
-                                foodSchedule: {
-                                    morning: "Some insects. 10 worms. 1/4 apple",
-                                    lunch: "Some insects. 10 worms. 1/4 apple",
-                                    evening: "Some insects. 10 worms. 1/4 apple"
-                                },
-                            },
-                            {
-                                min: 2
-                            }),
+                    body: [{
+                        name: like("Joy"),
+                        kind: "hedgehog",
+                        image: like({
+                            path: "/path/to/image"
+                        }),
+                        foodSchedule: like({
+                            morning: "Some insects. 10 worms. 1/4 apple",
+                            lunch: "Some insects. 10 worms. 1/4 apple",
+                            evening: "Some insects. 10 worms. 1/4 apple"
+                        }),
+                    }, {
+                        name: like("Joy"),
+                        kind: "hedgehog",
+                        image: like({
+                            path: "/path/to/image"
+                        }),
+                        foodSchedule: like({
+                            morning: "Some insects. 10 worms. 1/4 apple",
+                            lunch: "Some insects. 10 worms. 1/4 apple",
+                            evening: "Some insects. 10 worms. 1/4 apple"
+                        }),
+                    },
+                    ],
                     status: 200,
                     headers: {
                         'Content-Type': 'application/json',
@@ -51,9 +62,9 @@ describe('Contract with Service C', () => {
                     .then(() => provider.addInteraction(interaction));
         });
 
-        it('will receive a list of animals with pictures', () => {
-            return expect(fetchAnimals('hedgehog')).resolves.toIncludeSameMembers([ 
-                        expect.toContainAllKeys(['name', 'kind', 'image']),
+        it('will receive a list of hedgehogs with pictures', () => {
+            return expect(fetchAnimals('hedgehog')).resolves.toIncludeSameMembers([
+                        expect.toContainAllEntries([['name', expect.any(String)], ['kind', 'hedgehog'], ['image', expect.any(String)]]),
                         expect.toContainAllKeys(['name', 'kind', 'image']),
                     ]
             );
